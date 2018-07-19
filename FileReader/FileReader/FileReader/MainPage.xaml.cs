aad5861ada3ee37f32b242fc6a2e33f1181128d8
Kenneth Moody (Populus Group <v-kemood@microsoft.com>
2018-07-19 17:13:41 -0400

    First commit for reading files

---
+++ FileReader/FileReader/FileReader/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace FileReader
+{
+    /*
+     *This main page just is a way to test to see that the FileParser class works. 
+     * The main page creates a FileParser object and inputs 11 numbers. The MainPage() calls the FileToArray function and sets a label equal to that array.
+     * The app will not actually display the array correctly 
+     * Set a breakpoint on line 25 and then hover over DataInDoubleArray to see the items in the array.
+     */
+	public partial class MainPage : ContentPage
+	{
+        FileParser ObjToGetData = new FileParser("26,23.09,45.80,5,34,59,2,67.9,34.7,4.9,45.34");
+
+        public MainPage()
+		{
+			InitializeComponent();
+            double[] DataInDoubleArray = ObjToGetData.FileToArray();
+            SensorDataLabel.Text = DataInDoubleArray.ToString();
+            //System.Diagnostics.Debug.WriteLine("*********************************\n******************************************\n**********************************");
+            //System.Diagnostics.Debug.WriteLine(DataInDoubleArray + "");
+            //System.Diagnostics.Debug.WriteLine(DataInDoubleArray.ToString());
+            //System.Diagnostics.Debug.WriteLine(DataInDoubleArray.ToList().ToString());
+            //System.Diagnostics.Debug.WriteLine("*********************************\n******************************************\n**********************************");
+
+
+
+        }
+
+
+
+	}
+}
