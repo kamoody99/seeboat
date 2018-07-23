e38b1c5b93f0f194b9116c14857992e8ba4f0605
Kenneth Moody (Populus Group <v-kemood@microsoft.com>
2018-07-23 16:14:14 -0400

    This is an early untested version of pulling data from the cloud to the app

---
+++ PullFromCloudFeat/PullFromCloudFeat/PullFromCloudFeat.Android/obj/Debug/android/src/md5f1e69ce5ac77e5623d1e1d56d8550240/MainActivity.java
@@ -0,0 +1,47 @@
+package md5f1e69ce5ac77e5623d1e1d56d8550240;
+
+
+public class MainActivity
+	extends md51558244f76c53b6aeda52c8a337f2c37.FormsAppCompatActivity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"";
+		mono.android.Runtime.register ("PullFromCloudFeat.Droid.MainActivity, PullFromCloudFeat.Android", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity ()
+	{
+		super ();
+		if (getClass () == MainActivity.class)
+			mono.android.TypeManager.Activate ("PullFromCloudFeat.Droid.MainActivity, PullFromCloudFeat.Android", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
---
+++ PullFromCloudFeat/PullFromCloudFeat/PullFromCloudFeat/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace PullFromCloudFeat
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            CloudPull dataPullDown = new CloudPull();
+            System.Diagnostics.Debug.WriteLine("************************************/n***********************************/n******************************/n");
+            System.Diagnostics.Debug.WriteLine("" + dataPullDown.DoesTableExist());
+            System.Diagnostics.Debug.WriteLine("************************************/n***********************************/n******************************/n");
+        }
+    }
+}
